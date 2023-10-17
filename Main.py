cb7469064301659a06a4e4afa76d78f08447e4c0
vijaysanjana <anand123@purdue.edu>
2023-10-17 17:29:27 -0400

    attempting to correct 403 error

--- Main.py
+++ Main.py
@@ -1,8 +1,22 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup as soup
+from fake_useragent import UserAgent
 
-URL = "https://salemdeeds.com/salemdeeds/SaleSearch.aspx"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+user_agent = UserAgent()
 
-print(soup.find('td').get_text())
+headers={
+    'User-Agent': user_agent.random,
+}
+url = 'https://www.masslandrecords.com/BerkMiddle/'
+
+# Send a GET request, and automatically handle SSL certificate verification
+response = requests.get(url, headers=headers)
+
+
+if response.status_code == 200:
+    # Use BeautifulSoup to parse the HTML content
+    webpage = soup(response.content, 'html.parser')
+    # Now you can work with the webpage as needed
+    print(webpage.prettify())  # For example, print the prettified HTML
+else:
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
